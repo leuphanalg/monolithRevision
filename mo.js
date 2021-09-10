e98abf13477abf36dcdb63e7426d908b473af187
Monolith <83715191+leuphanalg@users.noreply.github.com>
2021-09-10 14:01:24 +0200

    Add files via upload

--- mo.js
+++ mo.js
@@ -1,8 +1,20 @@
 const burst = new mojs.Burst();
 
-document.addEventListener( 'click', function (e) {
+document.addEventListener( "click", function (e) {
   burst
-    //.tune({ x: e.pageX, y: e.pageY })
+    .tune({ x: e.pageX, y: e.pageY })
     .setSpeed(3)
     .replay();
+});
+
+const burst = new mojs.Burst({
+  radius:   { 0: 100 },
+  count:    5,
+  children: {
+    shape:        "circle",
+    radius:       20,
+    fill:         [ "skyblue", "cornflowerblue", "gray", "#b5bfff", "white" ],
+    strokeWidth:  5,
+    duration:     2000
+  }
 });
