1f301b4de6da4ac49e9035280d4bf243d245e25f
Monolith <83715191+leuphanalg@users.noreply.github.com>
2021-09-10 15:38:51 +0200

    Add files via upload

--- mousemotion.js
+++ mousemotion.js
@@ -42,4 +42,27 @@ document.addEventListener("dblclick", function(e){
   for(var i=0; i<ripples.length; i++){
     ripples[i].generate().replay();
   }
-})
+});
+
+const burst = new mojs.Burst({
+  radius:{
+    0:100
+  },
+  count:5,
+  left:0,
+  top:0,
+  children:{
+  shape:"circle",
+  radius:20,
+  fill:["skyblue", "cornflowerblue", "gray", "#b5bfff", "white"],
+  strokeWidth:5,
+  duration:2000
+  }
+});
+
+document.addEventListener("click", function (e){
+  burst.tune({
+    x: e.pageX,
+    y: e.pageY
+  }).setSpeed(3).replay();
+});
