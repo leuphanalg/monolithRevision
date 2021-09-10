8e577f877d43df8b2d00f2f92628ea1e79e01bb5
Monolith <83715191+leuphanalg@users.noreply.github.com>
2021-09-10 13:44:14 +0200

    Add files via upload

--- mo.js
+++ mo.js
@@ -1,12 +1,8 @@
 const burst = new mojs.Burst();
 
 document.addEventListener( 'click', function (e) {
-  burst.replay();
-});
-
-document.addEventListener( 'click', function (e) {
   burst
-    .tune({ x: e.pageX, y: e.pageY })
+    //.tune({ x: e.pageX, y: e.pageY })
     .setSpeed(3)
     .replay();
 });
